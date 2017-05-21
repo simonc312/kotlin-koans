0e40b66663959f06aa3632823e4f776c8c8a3c2e
Simon Chen <simon.c312@gmail.com>
2017-05-21 14:41:10 -0700

    finished i_introduction tests

--- src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -18,6 +18,12 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(o1: Int?, o2: Int?): Int {
+            if (o1 == o2) return 0
+            return o2?.compareTo(o1 ?: 1) ?: -1
+        }
+
+    })
     return arrayList
 }
